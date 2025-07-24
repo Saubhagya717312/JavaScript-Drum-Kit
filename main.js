ed7742be072d61a28076aceeb579e58b580b815a
Saubhagya Chaudhari <103045088+Saubhagya717312@users.noreply.github.com>
2025-07-25 00:06:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function removeTransition(e) {
+            const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+            if (key) {
+                key.classList.remove('playing');
+            }
+        }
+
+        function playSound(e) {
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+        if (!audio) return;
+
+        key.classList.add('playing');
+        audio.currentTime = 0;
+        audio.play();
+        }
+
+        window.addEventListener('keydown', playSound);
+        window.addEventListener('keyup', removeTransition);
